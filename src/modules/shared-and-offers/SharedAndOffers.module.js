3ca7f53ab78ac7ebe6d071dc5d92e6aa56a5c351
Alex <o.d.bilyi@gmail.com>
2018-09-12 09:55:48 +0300

    create shared and offers page

--- src/modules/shared-and-offers/SharedAndOffers.module.js
+++ src/modules/shared-and-offers/SharedAndOffers.module.js
@@ -5,7 +5,16 @@ export default class SharedAndOffers extends React.Component {
   render() {
     return(
       <Wrapper>
-        <div>Shared and Offers</div>
+        <div className="shared-and-offers-page">
+          <ul className="header-tab">
+            <li className="header-tab-item">
+              <button>акції</button>
+            </li>
+            <li className="header-tab-item">
+              <button>новинки</button>
+            </li>
+          </ul>
+        </div>
       </Wrapper>
     )
   }
